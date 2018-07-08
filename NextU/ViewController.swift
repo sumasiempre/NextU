03fd6e3629662cced8f5446d0cbe5a744457d113
Eduardo Seelnig <eduardo@som4ndo.com>
2018-07-08 16:31:06 -0400

    Initial Commit

---
+++ NextU/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  NextU
+//
+//  Created by Eduardo Seeling on 7/8/18.
+//  Copyright © 2018 SumaSiempre. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
